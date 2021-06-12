2677dc043e4237f90be61ddae488088b1e4ab130
Thiago <t.r.goveia@gmail.com>
2021-06-11 23:43:14 -0300

    Corrige interação da posição com a velocidade do herói

--- Trabalho1/Bomberman/src/Tools/Position.java
+++ Trabalho1/Bomberman/src/Tools/Position.java
@@ -7,21 +7,34 @@ public class Position {
     private int column;
 
     private int previousRow;
-    private int preveiousColumn;
+    private int previousColumn;
 
     public Position(int row, int column){
-        this.setPosition(row,column);
+        this.setPosition(row, column);
     }
 
     public boolean setPosition(int row, int column){
-        if(row < 0 || row >= Tools.Consts.RES * Consts.CELL_SIZE_FACTOR)
+        if(row < 0) {
+            this.row = 0;
             return false;
+        }
         previousRow = this.row;
+        if (row > (Tools.Consts.RES - 1) * Consts.CELL_SIZE_FACTOR) {
+            System.out.println(row);
+            this.row = (Tools.Consts.RES - 1) * Consts.CELL_SIZE_FACTOR;
+            return false;
+        }
         this.row = row;
 
-        if(column < 0 || column >= Tools.Consts.RES * Consts.CELL_SIZE_FACTOR)
+        previousColumn = this.column;
+        if(column < 0) {
+            this.column = 0;
             return false;
-        preveiousColumn = this.column;
+        }
+        if (column > (Tools.Consts.RES - 1) * Consts.CELL_SIZE_FACTOR) {
+            this.column = (Tools.Consts.RES - 1) * Consts.CELL_SIZE_FACTOR;
+            return false;
+        }
         this.column = column;
         return true;
     }
@@ -31,7 +44,7 @@ public class Position {
     }
 
     public boolean reset(){
-        return this.setPosition(previousRow, preveiousColumn);
+        return this.setPosition(previousRow, previousColumn);
     }
 
     public int getColumn(){
@@ -50,15 +63,15 @@ public class Position {
     }
 
     public boolean moveUp(){
-        return this.setPosition(this.getRow() - 1, this.getColumn());
+        return this.setPosition(this.getRow() - Consts.HERO_SPEED, this.getColumn());
     }
     public boolean moveDown(){
-        return this.setPosition(this.getRow() + 1, this.getColumn());
+        return this.setPosition(this.getRow() + Consts.HERO_SPEED, this.getColumn());
     }
     public boolean moveRight(){
-        return this.setPosition(this.getRow(), this.getColumn() + 1);
+        return this.setPosition(this.getRow(), this.getColumn() + Consts.HERO_SPEED);
     }
     public boolean moveLeft(){
-        return this.setPosition(this.getRow(), this.getColumn() - 1);
+        return this.setPosition(this.getRow(), this.getColumn() - Consts.HERO_SPEED);
     }
 }
