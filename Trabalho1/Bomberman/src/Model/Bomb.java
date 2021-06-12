face433f0b05bcff020dd5a26da27d146586a835
Thiago <t.r.goveia@gmail.com>
2021-06-12 15:07:30 -0300

    Adiciona timer para modificar transponibilidade da bomba após 2 segundos

--- Trabalho1/Bomberman/src/Model/Bomb.java
+++ Trabalho1/Bomberman/src/Model/Bomb.java
@@ -1,11 +1,32 @@
 package Model;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import Tools.Events.EventBus;
 import Tools.Position.Position;
 
 public class Bomb extends Element {
     public Bomb (EventBus eventBus) {
         super("bomb.png", eventBus);
+        this.canBePassedThrough = true;
+
+        schedulePropertyChange();
+    }
+
+    private void turnBlockable() {
+        this.canBePassedThrough = false;
+        System.out.println("BUM");
+    }
+
+    private void schedulePropertyChange() {
+        TimerTask task = new TimerTask() {
+            public void run() {
+                turnBlockable();
+            }
+        };
+        Timer time = new Timer();
+        time.schedule(task, 2000);
     }
 
 
