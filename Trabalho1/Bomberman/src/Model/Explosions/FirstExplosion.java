222c8d2663accac02fe542870fa70b4514583fe9
Thiago <t.r.goveia@gmail.com>
2021-06-13 19:59:44 -0300

    Remove delay de propagação

--- Trabalho1/Bomberman/src/Model/Explosions/FirstExplosion.java
+++ Trabalho1/Bomberman/src/Model/Explosions/FirstExplosion.java
@@ -1,29 +1,14 @@
 package Model.Explosions;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 import Model.Explosion;
 import Tools.Events.EventBus;
 import Tools.Image.Animator;
 
 public class FirstExplosion extends Explosion implements Propagable {
-    private Timer propagationTimer;
-
     public FirstExplosion (Animator animator, EventBus eventBus, int intensity) {
         super(animator, eventBus);
         this.intensity = intensity;
         propagateExplosion();
-    }
-
-    public void setPropagationTimer() {
-        TimerTask task = new TimerTask() {
-            public void run() {
-                propagateExplosion();
-            }
-        };
-        propagationTimer = new Timer();
-        propagationTimer.schedule(task, 50);
     }
 
     public void propagateExplosion() {
--- Trabalho1/Bomberman/src/Model/Explosions/MiddleExplosion.java
+++ Trabalho1/Bomberman/src/Model/Explosions/MiddleExplosion.java
@@ -1,26 +1,12 @@
 package Model.Explosions;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 import Model.Explosion;
 import Tools.Events.EventBus;
 import Tools.Image.Animator;
 
 public abstract class MiddleExplosion extends Explosion implements Propagable {
-    private Timer propagationTimer;
-
     public MiddleExplosion (Animator animator, EventBus eventBus) {
         super(animator, eventBus);
-    }
-
-    public void setPropagationTimer() {
-        TimerTask task = new TimerTask() {
-            public void run() {
-                propagateExplosion();
-            }
-        };
-        propagationTimer = new Timer();
-        propagationTimer.schedule(task, 50);
+        propagateExplosion();
     }
 }
