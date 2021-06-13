0977de918f686d59537ccbb46eae5669fc4de6ba
Thiago <t.r.goveia@gmail.com>
2021-06-13 17:17:22 -0300

    Adiciona  restrição à criação de um novo elemento, a posição desse elemento deve estar vazia.

--- Trabalho1/Bomberman/src/Tools/Events/CreateElementsEvent.java
+++ Trabalho1/Bomberman/src/Tools/Events/CreateElementsEvent.java
@@ -2,12 +2,18 @@ package Tools.Events;
 
 import Controls.Screen;
 import Model.Element;
+import Tools.InteractionMap;
 
 public class CreateElementsEvent implements Event {
 
     public void fire(Screen screen, Element... elements) {
         for (Element element: elements) {
-            screen.addElement(element);
+            InteractionMap interactionMap = screen.getInteractionMap();
+            if (interactionMap.get(element.getPosition()) == null) { // Somente criar o elemento se posição estiver vazia
+                System.out.println("ADD");
+                screen.addElement(element);
+                interactionMap.insert(element.getPosition(), element);
+            }
         }
     }
 }
