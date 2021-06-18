b3f1f146fe854d41e1768156b2b3226fad2a8e0e
Thiago <t.r.goveia@gmail.com>
2021-06-18 20:53:00 -0300

    Adiciona movimentação aos inimigos

--- Trabalho1/Bomberman/src/Model/Enemies/Enemy.java
+++ Trabalho1/Bomberman/src/Model/Enemies/Enemy.java
@@ -1,5 +1,6 @@
 package Model.Enemies;
 
+import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -8,9 +9,12 @@ import Model.Explosion;
 import Model.Hero;
 import Model.MovableElement;
 import Tools.Events.EventBus;
+import Tools.Position.HitBox;
 import Tools.Position.Position;
 
 public abstract class Enemy extends MovableElement {
+    private static String[] directions = {"up", "down", "right", "left"};
+
     protected Enemy(EventBus<Element>eventBus, Position position) {
         super(eventBus, position, 20);
     }
@@ -22,11 +26,99 @@ public abstract class Enemy extends MovableElement {
     }
 
     public void interact(Enemy enemy) {
-        // Turn back
+        enemy.changeDirection();
     }
 
     public void interact(Explosion explosion) {
         die();
+    }
+
+    public void processMovement() {
+        if (!Position.isPositionOutOfBoundaries(this.nextPosition)) {
+            this.setHitBox(
+                new HitBox(this.nextPosition)
+            );
+            this.eventBus.emit("verify-element-interaction", this);
+            if (this.interactingElement != null && this.interactingElement != this) {
+                this.interactingElement.interact(this);
+                this.changeDirection();
+                if (this.interactingElement.isTraversable()) {
+                    this.position.setPosition(this.nextPosition);
+                } else {
+                    this.setStopedAnimator();
+                }
+            } else {
+                this.position.setPosition(this.nextPosition);
+            }
+        } else {
+            this.changeDirection();
+        }
+    }
+
+    public void changeDirection() {
+        Random random = new Random();
+        int nextDirection = random.nextInt(4);
+        while (this.movementDirection == directions[nextDirection]) {
+            nextDirection = random.nextInt(4);
+        }
+        this.movementDirection = directions[nextDirection];
+        move();
+    }
+
+    protected void move() {
+        if (this.movementDirection == "up") {
+            this.moveUp();
+        } else if (this.movementDirection == "down") {
+            this.moveDown();
+        } else if (this.movementDirection == "left") {
+            this.moveLeft();
+        } else if (this.movementDirection == "right") {
+            this.moveRight();
+        }
+    }
+
+    public void moveUp() {
+        changeAnimatorAndKillMovement(upAnimator, "up");
+        this.movementTimer = new TimerTask() {
+            public void run() {
+                nextPosition = position.getMovementUp();
+                processMovement();
+            }
+        };
+        this.move(this.speed);
+    }
+
+    public void moveDown() {
+        changeAnimatorAndKillMovement(downAnimator, "down");
+        this.movementTimer = new TimerTask() {
+            public void run() {
+                nextPosition = position.getMovementDown();
+                processMovement();
+            }
+        };
+        this.move(this.speed);
+    }
+
+    public void moveRight() {
+        changeAnimatorAndKillMovement(rightAnimator, "right");
+        this.movementTimer = new TimerTask() {
+            public void run() {
+                nextPosition = position.getMovementRight();
+                processMovement();
+            }
+        };
+        this.move(this.speed);
+    }
+
+    public void moveLeft() {
+        changeAnimatorAndKillMovement(leftAnimator, "left");
+        movementTimer = new TimerTask() {
+            public void run() {
+                nextPosition = position.getMovementLeft();
+                processMovement();
+            }
+        };
+        this.move(this.speed);
     }
 
 
