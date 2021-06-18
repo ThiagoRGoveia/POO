2c1293027ff3b9669825b3c5648a297351cec66b
michel-palmezan <83554147+michel-palmezan@users.noreply.github.com>
2021-06-17 21:55:28 -0300

    classes abstratas criadas

--- Trabalho1/Bomberman/src/Tools/Image/Boundaries/BoundariesFactoryLevel2.java
+++ Trabalho1/Bomberman/src/Tools/Image/Boundaries/BoundariesFactoryLevel2.java
@@ -92,6 +92,8 @@ public class BoundariesFactoryLevel2 extends BoundaryFactory {
     public Boundaries[] getEnemyVerticalUpMovementBoundaries() {
         Boundaries[] b = {
             new Boundaries(128, 28, 16, 16),
+            new Boundaries(146, 28, 16, 16),
+            new Boundaries(128, 28, 16, 16),
             new Boundaries(146, 28, 16, 16)
         };
         return b;
@@ -99,6 +101,8 @@ public class BoundariesFactoryLevel2 extends BoundaryFactory {
 
     public Boundaries[] getEnemyVerticalDownMovementBoundaries() {
         Boundaries[] b = {
+            new Boundaries(92, 28, 16, 16),
+            new Boundaries(110, 28, 16, 16),
             new Boundaries(92, 28, 16, 16),
             new Boundaries(110, 28, 16, 16)
         };
@@ -108,6 +112,8 @@ public class BoundariesFactoryLevel2 extends BoundaryFactory {
     public Boundaries[] getEnemyHorizontalLeftMovementBoundaries() {
         Boundaries[] b = {
             new Boundaries(56, 28, 16, 16),
+            new Boundaries(74, 28, 16, 16),
+            new Boundaries(56, 28, 16, 16),
             new Boundaries(74, 28, 16, 16)
         };
         return b;
@@ -115,6 +121,8 @@ public class BoundariesFactoryLevel2 extends BoundaryFactory {
 
     public Boundaries[] getEnemyHorizontalRightMovementBoundaries() {
         Boundaries[] b = {
+            new Boundaries(303, 340, 16, 16),
+            new Boundaries(321, 340, 16, 16),
             new Boundaries(303, 340, 16, 16),
             new Boundaries(321, 340, 16, 16)
         };
--- Trabalho1/Bomberman/src/Tools/Image/Boundaries/BoundaryFactory.java
+++ Trabalho1/Bomberman/src/Tools/Image/Boundaries/BoundaryFactory.java
@@ -9,4 +9,8 @@ public abstract class BoundaryFactory {
     public abstract Boundaries[] getVerticalDownLastExplosionsionBoundaries();
     public abstract Boundaries[] getVerticalMiddleExplosionsionBoundaries();
     public abstract Boundaries[] getVerticalUpLastExplosionsionBoundaries();
+    public abstract Boundaries[] getEnemyVerticalUpMovementBoundaries();
+    public abstract Boundaries[] getEnemyVerticalDownMovementBoundaries();
+    public abstract Boundaries[] getEnemyHorizontalLeftMovementBoundaries();
+    public abstract Boundaries[] getEnemyHorizontalRightMovementBoundaries();
 }
