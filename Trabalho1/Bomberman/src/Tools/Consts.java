976042fac127ebb55425fd61b4f518dd4c7d5904
Thiago <t.r.goveia@gmail.com>
2021-06-18 22:23:17 -0300

    Ajusta CELL_SIZE_FACTOR

--- Trabalho1/Bomberman/src/Tools/Consts.java
+++ Trabalho1/Bomberman/src/Tools/Consts.java
@@ -12,7 +12,7 @@ public class Consts {
     public static final int FRAME_INTERVAL = 10;
     public static final int TIMER_DISPARO = 20; /*Em numero de frames (redesenhos)*/
     public static final String PATH = File.separator+"imgs"+File.separator;
-    public static final int CELL_SIZE_FACTOR = 50;
+    public static final int CELL_SIZE_FACTOR = 40;
     public static final int HERO_HIT_BOX_FACTOR = 3;
     public static final int SCREEN_BOUNDARY = Consts.CELL_SIZE_FACTOR * (Consts.RES - 1);
     public static final String SPRITE_NAME = "all.png";
