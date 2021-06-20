0e3fe6b110470203fcc53740e301ed5d36a309e2
Thiago <t.r.goveia@gmail.com>
2021-06-20 12:34:23 -0300

    Modifica verificação de interação entre elementos para conciderar dimenções da célula

--- Trabalho1/Bomberman/src/Tools/Events/VerifyElementInteractionEvent.java
+++ Trabalho1/Bomberman/src/Tools/Events/VerifyElementInteractionEvent.java
@@ -3,23 +3,47 @@ package Tools.Events;
 import Controls.Screen;
 import Model.Element;
 import Model.MovableElement;
+import Tools.Consts;
 import Tools.InteractionMap;
+import Tools.Position.Column;
 import Tools.Position.HitBox;
+import Tools.Position.Position;
+import Tools.Position.Row;
+import Tools.Position.ScreenPosition;
 
 public class VerifyElementInteractionEvent implements Event<Element>{
 
     public void fire(Screen screen, Element element) {
         MovableElement movableElement = (MovableElement) element;
         InteractionMap interactionMap = screen.getInteractionMap();
-        Element existingElement = interactionMap.get(movableElement.getNextPosition());
+        String movementDirection = movableElement.getMovementDirection();
+        int rowFactor = 0;
+        int colFactor = 0;
+        // Verificar qual a direção de movimento atual e corrigir posição
+        // com relação à fronteira da célula
+        if (movementDirection == "up") {
+            rowFactor = -Consts.RES / 2;
+        } else if (movementDirection == "down") {
+            rowFactor = Consts.RES / 2;
+        } else if (movementDirection == "left") {
+            colFactor = -Consts.RES / 2;
+        } else if (movementDirection == "right") {
+            colFactor = Consts.RES / 2;
+        }
+        // Verificar se existe algum elemento na proxima célula
+        Element existingElement = interactionMap.get(
+            new Position(
+                new Row(
+                    new ScreenPosition(movableElement.getNextPosition().getRow().getScreenPosition().value + rowFactor)
+                ),
+                new Column(
+                    new ScreenPosition(movableElement.getNextPosition().getColumn().getScreenPosition().value + colFactor)
+                )
+            )
+        );
+        // Se existir, informar ao elemento móvel da interação
         if (existingElement != null) {
-            boolean isHiting = HitBox.isHiting(
-                element.getHitBox(),
-                existingElement.getHitBox()
-            );
-            if (isHiting) {
-                movableElement.setInteractingElement(existingElement);
-            }
+            movableElement.setInteractingElement(existingElement);
         } else {
             movableElement.setInteractingElement(null);
         }
